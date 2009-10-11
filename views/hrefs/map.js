1d01add30fa711ae3c59985e55590281bb1babac
Chris Anderson <jchris@grabb.it>
2009-10-11 02:26:39 -0700

    view for inlinks

--- views/hrefs/map.js
+++ views/hrefs/map.js
@@ -3,18 +3,35 @@ function(doc) {
   // emit("ok",doc._id)
   if (doc && doc.form && doc.form.body) {
     var html = doc.form.body;
+    var url;
+    var t = false;
+    try {
+      HTMLParser(html, {
+        start: function( tag, attrs, unary ) {
+          if (tag == "a") {
+            for (var i=0; i < attrs.length; i++) {
+              if (attrs[i].name == "href") {
+                url = attrs[i].value
+                // emit(url,null)
+                t = true
+              }
+            };
+          }
+        },
+        end: function( tag ) {
+        },
+        chars: function( text ) {
+          if (t) emit(url, text);
+          t = false;
+        },
+        comment: function( text ) {
+        }
+      });
+      
+    } catch(e) {
+      emit("fail",doc.target)
+    }
     
-    HTMLParser(html, {
-      start: function( tag, attrs, unary ) {
-        emit(tag, attrs);
-      },
-      end: function( tag ) {
-      },
-      chars: function( text ) {
-      },
-      comment: function( text ) {
-      }
-    });
 
   }
 };
