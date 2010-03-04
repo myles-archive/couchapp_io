481585b7af53c8f504b2f4c28b56bf5d4c76485e
Myles Braithwaite <myles@yngvi.mylesbraithwaite.net>
2010-03-03 22:14:01 -0500

    Removed all the Jan references and replaced them with Myles.

--- shows/long.js
+++ shows/long.js
@@ -1,6 +1,6 @@
 function(doc, req) {
   if(!doc) {
-    return "Hey there, this is <a href=\"http://twitter.com/janl\">@janl's</a> personal URL shortener. Make your own with <a href=\"http://github.com/janl/io\">io</a>."
+    return "Hey there, this is <a href=\"http://twitter.com/mylesb\">@mylesb</a>'s personal URL shortener. Make your own with <a href=\"http://github.com/janl/io\">io</a>."
   }
 
   return {
